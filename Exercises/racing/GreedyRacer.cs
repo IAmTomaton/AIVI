84b9171ae65633f773cae439a2996446fdae3927
Jaggernaut09 <mrhomka.com@gmail.com>
2021-04-19 17:48:58 +0500

    GreedyRacer and Random

--- Exercises/racing/GreedyRacer.cs
+++ Exercises/racing/GreedyRacer.cs
@@ -9,7 +9,64 @@ namespace AiAlgorithms.racing
     {
         public IEnumerable<RaceSolution> GetSolutions(RaceState problem, Countdown countdown)
         {
-            yield break;
+            var car = problem.Car;
+            var distanceToFlag = problem.GetFlagFor(car).DistTo(car.Pos);
+            var depth = Math.Min(10, (int)distanceToFlag / 3 + 1);
+
+            var directions = new V[9];
+
+            var index = 0;
+            for (var dx = -1; dx < 2; dx++)
+            {
+                for (var dy = -1; dy < 2; dy++)
+                {
+                    directions[index] = new V(dx, dy);
+                    index++;
+                }
+            }
+
+            V bestCommand = null;
+            var value = 0.0;
+
+            foreach (var dir in directions)
+            {
+                var newValue = Simulation(problem.MakeCopy(), dir, depth);
+                if (bestCommand is null || value < newValue)
+                {
+                    value = newValue;
+                    bestCommand = dir;
+                }
+            }
+
+            foreach (var dir in directions)
+            {
+                if (bestCommand != dir)
+                {
+                    yield return new RaceSolution(Enumerable.Range(0, depth).Select(_ => dir).ToArray());
+                }
+            }
+
+            yield return new RaceSolution(Enumerable.Range(0, depth).Select(_ => bestCommand).ToArray());
+        }
+
+        private double Simulation(RaceState problem, V dir, int depth)
+        {
+            var flagCost = 1000;
+            var distanceCost = 1;
+
+            for (var i = 0; i < depth; i++)
+            {
+                problem.Car.NextCommand = dir;
+                problem.Tick();
+                if (!problem.Car.IsAlive)
+                    return double.NegativeInfinity;
+                if (problem.IsFinished)
+                    break;
+            }
+            var car = problem.Car;
+            var value = car.FlagsTaken * flagCost - car.Pos.DistTo(problem.GetFlagFor(car)) * distanceCost;
+
+            return value;
         }
     }
 }
--- Exercises/racing/NaiveRacer.cs
+++ Exercises/racing/NaiveRacer.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using AiAlgorithms.Algorithms;
 
 namespace AiAlgorithms.racing
@@ -8,8 +9,70 @@ namespace AiAlgorithms.racing
     {
         public IEnumerable<RaceSolution> GetSolutions(RaceState problem, Countdown countdown)
         {
-            var delta = problem.GetFlagFor(problem.Car) - problem.Car.Pos;
-            yield return new RaceSolution(new[] {new V(Math.Sign(delta.X), Math.Sign(delta.Y))});
+            var car = problem.Car;
+            var distanceToFlag = problem.GetFlagFor(car).DistTo(car.Pos);
+            var depth = 5;
+
+            var directions = new V[9];
+
+            var index = 0;
+            for (var dx = -1; dx < 2; dx++)
+            {
+                for (var dy = -1; dy < 2; dy++)
+                {
+                    directions[index] = new V(dx, dy);
+                    index++;
+                }
+            }
+
+            var pathCount = 100;
+            var paths = new List<V[]>();
+            var random = new Random();
+
+            var bestPathIndex = 0;
+            var value = double.NegativeInfinity;
+
+            for (var i = 0; i < pathCount; i++)
+            {
+                var path = new V[depth].Select(_ => directions[random.Next(0, directions.Length)]).ToArray();
+                paths.Add(path);
+                var newValue = Simulation(problem.MakeCopy(), path);
+                if (value < newValue)
+                {
+                    value = newValue;
+                    bestPathIndex = i;
+                }
+            }
+
+            for (var i = 0; i < pathCount; i++)
+            {
+                if (bestPathIndex != i)
+                {
+                    yield return new RaceSolution(paths[i]);
+                }
+            }
+
+            yield return new RaceSolution(paths[bestPathIndex]);
+        }
+
+        private double Simulation(RaceState problem, V[] commands)
+        {
+            var flagCost = 1000;
+            var distanceCost = 1;
+
+            foreach (var command in commands)
+            {
+                problem.Car.NextCommand = command;
+                problem.Tick();
+                if (!problem.Car.IsAlive)
+                    return double.NegativeInfinity;
+                if (problem.IsFinished)
+                    break;
+            }
+            var car = problem.Car;
+            var value = car.FlagsTaken * flagCost - car.Pos.DistTo(problem.GetFlagFor(car)) * distanceCost;
+
+            return value;
         }
     }
 }
